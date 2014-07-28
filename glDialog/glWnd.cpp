57482ba60ab4462619cdea0fa85c485a4071376d
soulsheng <soulsheng@qq.com>
2014-07-28 15:34:57 +0800

    add response function for message WM_CREATE and WM_PAINT

--- glDialog/glWnd.cpp
+++ glDialog/glWnd.cpp
@@ -21,6 +21,8 @@ glWnd::~glWnd()
 
 
 BEGIN_MESSAGE_MAP(glWnd, CWnd)
+	ON_WM_CREATE()
+	ON_WM_PAINT()
 END_MESSAGE_MAP()
 
 
@@ -28,3 +30,22 @@ END_MESSAGE_MAP()
 // glWnd 消息处理程序
 
 
+
+
+int glWnd::OnCreate(LPCREATESTRUCT lpCreateStruct)
+{
+	if (CWnd::OnCreate(lpCreateStruct) == -1)
+		return -1;
+
+	// TODO:  在此添加您专用的创建代码
+
+	return 0;
+}
+
+
+void glWnd::OnPaint()
+{
+	CPaintDC dc(this); // device context for painting
+	// TODO: 在此处添加消息处理程序代码
+	// 不为绘图消息调用 CWnd::OnPaint()
+}
--- glDialog/glWnd.h
+++ glDialog/glWnd.h
@@ -13,6 +13,9 @@ public:
 
 protected:
 	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg int OnCreate(LPCREATESTRUCT lpCreateStruct);
+	afx_msg void OnPaint();
 };
 
 
