2a15a2036c281a011ed8c4a5f7b0a1cb97c60dd5
soulsheng <soulsheng@qq.com>
2014-07-28 15:31:48 +0800

    add a new MFC class inherited from CWnd

---
+++ glDialog/glWnd.cpp
@@ -0,0 +1,30 @@
+// glWnd.cpp : 实现文件
+//
+
+#include "stdafx.h"
+#include "glDialog.h"
+#include "glWnd.h"
+
+
+// glWnd
+
+IMPLEMENT_DYNAMIC(glWnd, CWnd)
+
+glWnd::glWnd()
+{
+
+}
+
+glWnd::~glWnd()
+{
+}
+
+
+BEGIN_MESSAGE_MAP(glWnd, CWnd)
+END_MESSAGE_MAP()
+
+
+
+// glWnd 消息处理程序
+
+
---
+++ glDialog/glWnd.h
@@ -0,0 +1,18 @@
+#pragma once
+
+
+// glWnd
+
+class glWnd : public CWnd
+{
+	DECLARE_DYNAMIC(glWnd)
+
+public:
+	glWnd();
+	virtual ~glWnd();
+
+protected:
+	DECLARE_MESSAGE_MAP()
+};
+
+
