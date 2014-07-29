be56c6ef7fec61b76d5e3f6f2045f46471edfb0a
soulsheng <soulsheng@qq.com>
2014-07-29 15:52:03 +0800

    add class Vector2

---
+++ glDialog/utility/vgfVector2.cpp
@@ -0,0 +1,26 @@
+/*
+-----------------------------------------------------------------------------
+  VRES(Virtual Reality GIS Engine System)    
+  -------------------------------------------------------------
+  License details ... waiting to add...
+  -------------------------------------------------------------
+  Copyright  (C) 2007 , leven - All Rights Reserved
+-----------------------------------------------------------------------------
+*/
+
+#include "vgfStableHeaders.h"
+#include "vgfVector2.h"
+#include "StdAfx.h"
+
+namespace vgFoundationSystem {
+	
+	const Vector2 Vector2::ZERO( 0, 0);
+
+	const Vector2 Vector2::UNIT_X( 1, 0);
+	const Vector2 Vector2::UNIT_Y( 0, 1);
+	const Vector2 Vector2::NEGATIVE_UNIT_X( -1,  0);
+	const Vector2 Vector2::NEGATIVE_UNIT_Y(  0, -1);
+	const Vector2 Vector2::UNIT_SCALE(1, 1);
+	
+	
+}
