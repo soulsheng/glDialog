9e2ba319e7d50a39ab35063a47cb5355e2ef7055
soulsheng <soulsheng@qq.com>
2014-07-30 10:41:15 +0800

    manager model object , read / render / cleanup

--- glDialog/glWnd.cpp
+++ glDialog/glWnd.cpp
@@ -34,6 +34,7 @@ glWnd::glWnd()
 
 glWnd::~glWnd()
 {
+	cleanup();
 	wglMakeCurrent(NULL,NULL);
 	wglDeleteContext(hglrc);//删除渲染描述表
 	::ReleaseDC(m_hWnd,hdc);//释放设备描述表
@@ -94,7 +95,7 @@ void glWnd::OnPaint()
 		g_look[0],	g_look[1],	g_look[2],
 		g_up[0],	g_up[1],	g_up[2]);
 
-
+	renderObject();
 #if 0
 	s+=0.005;
 	if(s>1.0)
@@ -239,4 +240,124 @@ void glWnd::DrawColorBox(void)
 
 	glEnd();
 
+}
+
+void glWnd::LoadModel( std::string filename )
+{
+
+	CFile fp;
+
+	int numOfObj;
+
+	long *ObjPosIndex;
+
+	vgObject *pobj;
+
+	if (!fp.Open(filename.c_str() , CFile::modeRead))
+	{
+		ostringstream ErrorMes;
+
+		ErrorMes<<filename.c_str()<<"open failed!";
+
+		AfxMessageBox(ErrorMes.str().c_str());
+
+		return;
+	}
+
+	///////////////文件格式///////////////////
+	char	fileFormat[32]="";
+	fp.Read( fileFormat, 32);
+	char	fmt[6]="";
+	strncpy( fmt, fileFormat, 5);
+	assert ( !strcmp( fmt, "VRGIS") );
+
+	// 文件版本
+	long		fileVersion;
+	fp.Read( &fileVersion, sizeof(long) );
+	assert ( 100 == fileVersion );
+	///////////////////////////////////////
+
+	fp.Read(&numOfObj , sizeof(int));
+
+	ObjPosIndex = new long[numOfObj];
+
+	fp.Read(ObjPosIndex , sizeof(long) * numOfObj);
+
+	////////////////////////以上读入文件头，以下读入obj数据////////////////////////
+
+	for (int i = 0; i < numOfObj ; i++)
+	{
+
+		pobj = new vgObject;
+
+		long objchunklength;
+
+		long pos = 0;
+
+		fp.Seek(ObjPosIndex[i] , CFile::begin);
+
+		fp.Read(&objchunklength , sizeof(long));
+
+		char *objdata = new char[objchunklength];
+
+		fp.Read(objdata , objchunklength);
+		////////////////////////////////////////////////////////////////
+		// 		IndexObject->m_id = GetNodeId()/*i+nodenumold*/;
+		// 
+		// 		IndexObject->m_isInMemory = true;
+
+		pobj->m_BoundryBox.maxPoint.x = *(float *)(objdata + pos);
+		pos += sizeof(float);
+
+		pobj->m_BoundryBox.minPoint.x = *(float *)(objdata + pos);
+		pos += sizeof(float);
+
+		pobj->m_BoundryBox.maxPoint.y = *(float *)(objdata + pos);
+		pos += sizeof(float);
+
+		pobj->m_BoundryBox.minPoint.y = *(float *)(objdata + pos);
+		pos += sizeof(float);
+
+		pobj->m_BoundryBox.maxPoint.z = *(float *)(objdata + pos);
+		pos += sizeof(float);
+
+		pobj->m_BoundryBox.minPoint.z = *(float *)(objdata + pos);
+		pos += sizeof(float);
+		/////////////////////////////给位置赋值/////////////////////////////////
+		pobj->UpdatePos();
+
+		pobj->DateObj(objdata + pos);
+
+		AddObject(pobj);
+
+		delete[] objdata;
+
+		objdata = NULL;
+	}
+
+	fp.Close();
+
+
+}
+
+void glWnd::AddObject( vgObject* pObject )
+{
+	m_objects.push_back( pObject );
+}
+
+void glWnd::renderObject()
+{
+	for ( int i=0; i<m_objects.size(); i++ )
+	{
+		m_objects[i]->Render();
+	}
+}
+
+void glWnd::cleanup()
+{
+	for ( int i=0; i<m_objects.size(); i++ )
+	{
+		delete m_objects[i];
+	}
+	m_objects.clear();
 }
--- glDialog/glWnd.h
+++ glDialog/glWnd.h
@@ -1,6 +1,11 @@
 #pragma once
 
 #include <gl/GL.h>
+#include "vgPrerequisites.h"
+
+#include "vgObjNode.h"
+
+using namespace vgFoundationSystem;
 
 // glWnd
 
@@ -13,6 +18,11 @@ public:
 	virtual ~glWnd();
 
 	// add member function and variable
+	void LoadModel( std::string filename );
+	void AddObject( vgObject* pObject );
+	void renderObject( );
+	void cleanup();
+
 	int MySetPixelFormat(HDC hdc);
 	void DrawColorBox(void);
 	HDC hdc;
@@ -23,6 +33,8 @@ public:
 	float	g_eye[3];		//	视点位置
 	float	g_look[3];		//	焦点位置
 	float	g_up[3];		//	头顶法向
+
+	std::vector<vgObject*>	m_objects;
 
 protected:
 	DECLARE_MESSAGE_MAP()
